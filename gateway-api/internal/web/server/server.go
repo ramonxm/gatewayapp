08a6f3668d3ce4239a7520c3f95f4aa0783f9535
Ramon Xavier <ramon1234rxm@gmail.com>
2025-04-07 19:27:13 -0300

    feat(server): add server setup with account routes
    Introduce a new server package to handle HTTP requests. The server is configured with routes for account-related operations, including creating and retrieving accounts. This setup is essential for enabling the gateway API to manage account interactions.

---
+++ gateway-api/internal/web/server/server.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/ramonxm/gatewayapp/gateway-api/internal/service"
+	"github.com/ramonxm/gatewayapp/gateway-api/internal/web/handlers"
+)
+
+type Server struct {
+	router         *chi.Mux
+	server         *http.Server
+	accountService *service.AccountService
+	port           string
+}
+
+func NewServer(port string, accountService *service.AccountService) *Server {
+	return &Server{
+		router:         chi.NewRouter(),
+		accountService: accountService,
+		port:           port,
+	}
+}
+
+func (s *Server) ConfigureRoutes() {
+	accountHandler := handlers.NewAccountHandler(s.accountService)
+
+	s.router.Post("/accounts", accountHandler.Create)
+	s.router.Get("/accounts", accountHandler.Get)
+}
+
+func (s *Server) Start() error {
+	s.server = &http.Server{
+		Addr:    ":" + s.port,
+		Handler: s.router,
+	}
+
+	return s.server.ListenAndServe()
+}
